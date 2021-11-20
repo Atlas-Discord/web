3f222693824db2280faae83c6e5b88205e020be0
Victor A. Seixas <Victors3ixas@gmail.com>
2021-11-19 23:11:37 -0300

    reajustes

--- javascript/init.js
+++ javascript/init.js
@@ -3,132 +3,123 @@ window.onload = initPage;
 function initPage() {
     header_genessis();
     footer_genessis();
-
-    var inicio_content = "Inicio";
-    var equipe_content = 'Equipe';
-    var blog_content = 'Blog';
-    var suporte_content = 'Suporte';
-    var incio_href = "#";
-    var equipe_href = "#";
-    var blog_href = "#";
-    var suporte_href = "#";
     
     function header_genessis() {
-    __collapse_genessis();
-    __content_genessis();
+        __collapse_genessis();
+        __content_genessis();
 
-    function __collapse_genessis() {
-        header = document.querySelector('header');
-        
-        var collapse = document.createElement('nav');
-        var collapse_header = document.createElement('div');
-        var collapse_content = document.createElement('div');
-        var collapse_header_close = document.createElement('button');
-        var collapse_content_ul = document.createElement('ul');
-        var collapse_content_ul_li_1 = document.createElement('li');
-        var collapse_content_ul_li_2 = document.createElement('li');
-        var collapse_content_ul_li_3 = document.createElement('li');
-        var collapse_content_ul_li_4 = document.createElement('li');
-        var collapse_content_ul_li_1_a = document.createElement('a');
-        var collapse_content_ul_li_2_a = document.createElement('a');  
-        var collapse_content_ul_li_3_a = document.createElement('a');
-        var collapse_content_ul_li_4_a = document.createElement('a');
+        function __collapse_genessis() {
+            header = document.querySelector('header');
             
+            var collapse = document.createElement('nav');
+            var collapse_header = document.createElement('div');
+            var collapse_content = document.createElement('div');
+            var collapse_header_close = document.createElement('button');
+            var collapse_content_ul = document.createElement('ul');
+            var collapse_content_ul_li_1 = document.createElement('li');
+            var collapse_content_ul_li_2 = document.createElement('li');
+            var collapse_content_ul_li_3 = document.createElement('li');
+            var collapse_content_ul_li_4 = document.createElement('li');
+            var collapse_content_ul_li_1_a = document.createElement('a');
+            var collapse_content_ul_li_2_a = document.createElement('a');  
+            var collapse_content_ul_li_3_a = document.createElement('a');
+            var collapse_content_ul_li_4_a = document.createElement('a');
+                
 
-        collapse.classList.add('collapse-navbar');
-        collapse.setAttribute('id', 'header-collapse-navbar');
-        collapse.setAttribute('data-toggle', 'false');
-        collapse_header.classList.add('header-collapse');
-        collapse_content.classList.add('content-collapse');
-        collapse_header_close.classList.add('btn-close');
-        collapse_header_close.setAttribute('onclick', 'header_collapse()'); 
-        collapse_content_ul_li_1_a.href = '#';
-        collapse_content_ul_li_1_a.classList.add('btn');
-        collapse_content_ul_li_1_a.textContent = 'Inicio';
-        collapse_content_ul_li_2_a.href = '#';
-        collapse_content_ul_li_2_a.classList.add('btn');
-        collapse_content_ul_li_2_a.textContent = 'Equipe';
-        collapse_content_ul_li_3_a.href = '#';
-        collapse_content_ul_li_3_a.classList.add('btn');
-        collapse_content_ul_li_3_a.textContent = 'Blog';
-        collapse_content_ul_li_4_a.href = '#';
-        collapse_content_ul_li_4_a.classList.add('btn');
-        collapse_content_ul_li_4_a.textContent = 'Suporte';
+            collapse.classList.add('collapse-navbar');
+            collapse.setAttribute('id', 'header-collapse-navbar');
+            collapse.setAttribute('data-toggle', 'false');
+            collapse_header.classList.add('header-collapse');
+            collapse_content.classList.add('content-collapse');
+            collapse_header_close.classList.add('btn-close');
+            collapse_header_close.setAttribute('onclick', 'header_collapse()'); 
+            collapse_content_ul_li_1_a.href = 'index.html';
+            collapse_content_ul_li_1_a.classList.add('btn');
+            collapse_content_ul_li_1_a.textContent = 'Inicio';
+            collapse_content_ul_li_2_a.href = '#';
+            collapse_content_ul_li_2_a.classList.add('btn');
+            collapse_content_ul_li_2_a.textContent = 'Equipe';
+            collapse_content_ul_li_3_a.href = '#';
+            collapse_content_ul_li_3_a.classList.add('btn');
+            collapse_content_ul_li_3_a.textContent = 'Blog';
+            collapse_content_ul_li_4_a.href = 'suporte-page.html';
+            collapse_content_ul_li_4_a.classList.add('btn');
+            collapse_content_ul_li_4_a.textContent = 'Suporte';
 
-        header.appendChild(collapse);
-        collapse.appendChild(collapse_header);
-        collapse.appendChild(collapse_content);
-        collapse_header.appendChild(collapse_header_close);
-        collapse_content.appendChild(collapse_content_ul);
-        collapse_content_ul.appendChild(collapse_content_ul_li_1);
-        collapse_content_ul.appendChild(collapse_content_ul_li_2);
-        collapse_content_ul.appendChild(collapse_content_ul_li_3);
-        collapse_content_ul.appendChild(collapse_content_ul_li_4);
-        collapse_content_ul_li_1.appendChild(collapse_content_ul_li_1_a);
-        collapse_content_ul_li_2.appendChild(collapse_content_ul_li_2_a);
-        collapse_content_ul_li_3.appendChild(collapse_content_ul_li_3_a);
-        collapse_content_ul_li_4.appendChild(collapse_content_ul_li_4_a);
+            header.appendChild(collapse);
+            collapse.appendChild(collapse_header);
+            collapse.appendChild(collapse_content);
+            collapse_header.appendChild(collapse_header_close);
+            collapse_content.appendChild(collapse_content_ul);
+            collapse_content_ul.appendChild(collapse_content_ul_li_1);
+            collapse_content_ul.appendChild(collapse_content_ul_li_2);
+            collapse_content_ul.appendChild(collapse_content_ul_li_3);
+            collapse_content_ul.appendChild(collapse_content_ul_li_4);
+            collapse_content_ul_li_1.appendChild(collapse_content_ul_li_1_a);
+            collapse_content_ul_li_2.appendChild(collapse_content_ul_li_2_a);
+            collapse_content_ul_li_3.appendChild(collapse_content_ul_li_3_a);
+            collapse_content_ul_li_4.appendChild(collapse_content_ul_li_4_a);
 
-    }
+        }
 
-    function __content_genessis() {
-        header = document.querySelector('header');
+        function __content_genessis() {
+            header = document.querySelector('header');
 
-        var content = document.createElement('nav');
-        var content_logo = document.createElement('span');
-        var content_ul_1 = document.createElement('ul');
-        var content_ul_2 = document.createElement('ul');
-        var content_ul_1_li_1 = document.createElement('li');
-        var content_ul_1_li_2 = document.createElement('li');
-        var content_ul_1_li_3 = document.createElement('li');
-        var content_ul_1_li_4 = document.createElement('li');
-        var content_ul_2_li_1 = document.createElement('li');
-        var content_ul_1_li_1_a = document.createElement('a');
-        var content_ul_1_li_2_a = document.createElement('a');
-        var content_ul_1_li_3_a = document.createElement('a');
-        var content_ul_1_li_4_a = document.createElement('a');
-        var content_ul_2_li_1_button = document.createElement('button');
-
-
-        content.classList.add('content');
-        content.classList.add('m-1');            
-        content_logo.classList.add('logo')
-        content_logo.textContent = 'ATLAS';
-        content_ul_1.setAttribute('id', 'lg-navbar');
-        content_ul_2.setAttribute('id', 'sm-navbar');
-        content_ul_1_li_1_a.href = "#"
-        content_ul_1_li_1_a.classList.add('btn');
-        content_ul_1_li_1_a.textContent = `Incio`;
-        content_ul_1_li_2_a.href = "#"
-        content_ul_1_li_2_a.classList.add('btn');
-        content_ul_1_li_2_a.textContent = 'Equipe'
-        content_ul_1_li_3_a.href = "#"
-        content_ul_1_li_3_a.classList.add('btn');
-        content_ul_1_li_3_a.textContent = 'Blog'
-        content_ul_1_li_4_a.href = "#"
-        content_ul_1_li_4_a.classList.add('btn');
-        content_ul_1_li_4_a.textContent = 'Suporte'
-        content_ul_2_li_1_button.classList.add('btn');
-        content_ul_2_li_1_button.classList.add('btn-outline-primary');
-        content_ul_2_li_1_button.setAttribute('onclick', 'header_collapse()');
-        content_ul_2_li_1_button.textContent = 'Mais..';
+            var content = document.createElement('nav');
+            var content_logo = document.createElement('span');
+            var content_ul_1 = document.createElement('ul');
+            var content_ul_2 = document.createElement('ul');
+            var content_ul_1_li_1 = document.createElement('li');
+            var content_ul_1_li_2 = document.createElement('li');
+            var content_ul_1_li_3 = document.createElement('li');
+            var content_ul_1_li_4 = document.createElement('li');
+            var content_ul_2_li_1 = document.createElement('li');
+            var content_ul_1_li_1_a = document.createElement('a');
+            var content_ul_1_li_2_a = document.createElement('a');
+            var content_ul_1_li_3_a = document.createElement('a');
+            var content_ul_1_li_4_a = document.createElement('a');
+            var content_ul_2_li_1_button = document.createElement('button');
 
 
-        header.appendChild(content);
-        content.appendChild(content_logo);
-        content.appendChild(content_ul_1);
-        content.appendChild(content_ul_2);
-        content_ul_1.appendChild(content_ul_1_li_1);
-        content_ul_1.appendChild(content_ul_1_li_2);
-        content_ul_1.appendChild(content_ul_1_li_3);
-        content_ul_1.appendChild(content_ul_1_li_4);
-        content_ul_2.appendChild(content_ul_2_li_1);
-        content_ul_1_li_1.appendChild(content_ul_1_li_1_a);
-        content_ul_1_li_2.appendChild(content_ul_1_li_2_a);
-        content_ul_1_li_3.appendChild(content_ul_1_li_3_a);
-        content_ul_1_li_4.appendChild(content_ul_1_li_4_a);
-        content_ul_2_li_1.appendChild(content_ul_2_li_1_button);
-    }  
+            content.classList.add('content');
+            content.classList.add('m-1');            
+            content_logo.classList.add('logo')
+            content_logo.textContent = 'ATLAS';
+            content_ul_1.setAttribute('id', 'lg-navbar');
+            content_ul_2.setAttribute('id', 'sm-navbar');
+            content_ul_1_li_1_a.href = "index.html"
+            content_ul_1_li_1_a.classList.add('btn');
+            content_ul_1_li_1_a.textContent = `Incio`;
+            content_ul_1_li_2_a.href = "#"
+            content_ul_1_li_2_a.classList.add('btn');
+            content_ul_1_li_2_a.textContent = 'Equipe'
+            content_ul_1_li_3_a.href = "#"
+            content_ul_1_li_3_a.classList.add('btn');
+            content_ul_1_li_3_a.textContent = 'Blog'
+            content_ul_1_li_4_a.href = "suporte-page.html"
+            content_ul_1_li_4_a.classList.add('btn');
+            content_ul_1_li_4_a.textContent = 'Suporte'
+            content_ul_2_li_1_button.classList.add('btn');
+            content_ul_2_li_1_button.classList.add('btn-outline-primary');
+            content_ul_2_li_1_button.setAttribute('onclick', 'header_collapse()');
+            content_ul_2_li_1_button.textContent = 'Mais..';
+
+
+            header.appendChild(content);
+            content.appendChild(content_logo);
+            content.appendChild(content_ul_1);
+            content.appendChild(content_ul_2);
+            content_ul_1.appendChild(content_ul_1_li_1);
+            content_ul_1.appendChild(content_ul_1_li_2);
+            content_ul_1.appendChild(content_ul_1_li_3);
+            content_ul_1.appendChild(content_ul_1_li_4);
+            content_ul_2.appendChild(content_ul_2_li_1);
+            content_ul_1_li_1.appendChild(content_ul_1_li_1_a);
+            content_ul_1_li_2.appendChild(content_ul_1_li_2_a);
+            content_ul_1_li_3.appendChild(content_ul_1_li_3_a);
+            content_ul_1_li_4.appendChild(content_ul_1_li_4_a);
+            content_ul_2_li_1.appendChild(content_ul_2_li_1_button);
+        }  
     }
 
     function footer_genessis() {
@@ -151,6 +142,8 @@ function initPage() {
             footer_div_1_p.textContent = 'ATLAS';
             footer_div_1_p.classList.add('logo-name');
         var footer_div_1_a = document.createElement('a');
+            footer_div_1_a.setAttribute('target', 'blank');
+            footer_div_1_a.href = 'https://discord.gg/8H5qrftdcf';
             footer_div_1_a.textContent = 'ENTRAR';
             footer_div_1_a.classList.add('btn');
             footer_div_1_a.classList.add('btn-primary');
@@ -161,7 +154,8 @@ function initPage() {
         var footer_div_3_span = document.createElement('span');
             footer_div_3_span.classList.add('copyright-name');
         var footer_div_4_a = document.createElement('a');
-            footer_div_4_a.href = '#';
+            footer_div_4_a.setAttribute('target', 'blank');
+            footer_div_4_a.href = 'https://victors3ixas.github.io/web/';
             footer_div_4_a.classList.add('btn');
             footer_div_4_a.classList.add('btn-outline-light');
             footer_div_4_a.classList.add('btn-sm');
@@ -188,7 +182,7 @@ function initPage() {
         var footer_div_2_ul_3_li_2 = document.createElement('li');
     
         var footer_div_2_ul_1_li_1_a = document.createElement('a');
-            footer_div_2_ul_1_li_1_a.href = '#';
+            footer_div_2_ul_1_li_1_a.href = 'index.html';
             footer_div_2_ul_1_li_1_a.textContent ='Inicio';
         var footer_div_2_ul_1_li_2_a = document.createElement('a');
             footer_div_2_ul_1_li_2_a.href = '#';
@@ -197,7 +191,7 @@ function initPage() {
             footer_div_2_ul_1_li_3_a.href = '#';
             footer_div_2_ul_1_li_3_a.textContent ='Blog';
         var footer_div_2_ul_1_li_4_a = document.createElement('a');
-            footer_div_2_ul_1_li_4_a.href = '#';
+            footer_div_2_ul_1_li_4_a.href = 'suporte-page.html';
             footer_div_2_ul_1_li_4_a.textContent ='Suporte';
         var footer_div_2_ul_2_li_1_span = document.createElement('span');
             footer_div_2_ul_2_li_1_span.textContent = 'VICTOR#1562';
@@ -206,10 +200,12 @@ function initPage() {
         var footer_div_2_ul_2_li_3_span = document.createElement('span');
             footer_div_2_ul_2_li_3_span.textContent = 'Picutinha#0541';
         var footer_div_2_ul_3_li_1_a = document.createElement('a');
-            footer_div_2_ul_3_li_1_a.href = '#';
+            footer_div_2_ul_3_li_1_a.href = 'https://squarecloud.app/';
+            footer_div_2_ul_3_li_1_a.setAttribute('target', 'blank')
             footer_div_2_ul_3_li_1_a.textContent = 'SquareCloud';
         var footer_div_2_ul_3_li_2_a = document.createElement('a');
-            footer_div_2_ul_3_li_2_a.href = '#';
+            footer_div_2_ul_3_li_2_a.href = 'https://victors3ixas.github.io/web/';
+            footer_div_2_ul_3_li_2_a.setAttribute('target', 'blank')
             footer_div_2_ul_3_li_2_a.textContent = 'VictorSeixas';
     
     
